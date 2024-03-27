57a44fedaee8ee9aaf505c376991be84365f6564
adiazt01 <adiaztovar2004@gmail.com>
2024-03-26 21:38:19 -0400

    Update Navbar styling and add new charts to Activitys section

---
+++ src/assets/practice/index.js
@@ -0,0 +1,3 @@
+export { default as GraficoDeBarras } from './grafico-de-barras.png';
+export { default as GraficoDeDispersion } from './grafico-de-dispersion.png';
+export { default as GraficoDeCalor} from './grafico-de-calor.png';
